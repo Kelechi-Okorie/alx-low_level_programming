388f7a8ef0a7615875c1ac41605d2d1f993803f3
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-14 14:00:23 -0700

    Add 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: Prints the last digit
+ * of n with result
+ * of comparing this last digit
+ * with 5, 0, and 6
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last_digit = n % 10;
+
+	printf("%s%d%s%d", "Last digit of ", n, " is ", last_digit);
+
+	if (last_digit > 5)
+		printf(" and is greate than 5\n");
+	else if (last_digit == 0)
+		printf(" and is 0\n");
+	else if ((last_digit < 6) && (last_digit != 0))
+		printf(" and is less than 6 and not 0\n");
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,4 @@
 ## README file for 0x00-variables_if_else_while project
 File 0-positive_or_negative.c - A C program to check if the number n is positive or negative
+File 1-last_digit.c - A C program that prints the last digit of n and the result of a logical
+comparison this last digit with 5, 0 and 6
