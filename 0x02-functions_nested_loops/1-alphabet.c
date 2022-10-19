6f30577254656d6405e39ea211ea48e16f895477
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-19 07:28:48 -0700

    add 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 ## Readme file for 0x02-functions_nested_loops project
 
 File 0-putchar.c - A C program that prints _putchar
+File 1-alphabet.c - A C program that prints the alphabet in lowercase
