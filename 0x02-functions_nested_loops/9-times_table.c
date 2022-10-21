2349acc39e1ffa927924912fdb6716ae0268482b
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-21 05:39:37 -0700

    Bugfix 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,29 +9,30 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int r;
-	int s;
-	int t;
+	int k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			r = i * j;
-			s = r / 10;
-			t = r % 10;
+			k = i * j;
 
-			if (s != 0)
-				_putchar(s + '0');
-			else
-				_putchar(' ');
+			if (j == 0)
+				_putchar(k + '0');
 
-			_putchar(t + '0');
-
-			if (j != 9)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0);
 			}
 		}
 
