bb5dce89e61d428d4594e15185da7ba87171948e
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-22 11:54:50 -0700

    Bugfix on 0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-/* test comment */
 void positive_or_negative(int i);
 
-#endif /* _MAIN_H */
+#endif /* MAIN_H */
