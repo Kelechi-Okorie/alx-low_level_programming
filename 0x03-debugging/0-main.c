396e6b15b1e62dd202f436b8fc1b8a1bb05768c3
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-22 11:51:29 -0700

    Bugfix 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer
- * is positive or negative
+ * main - Entry point
+ * Description: Tests positve_or_negative function
  *
  * Return: Always 0 (success)
  */
