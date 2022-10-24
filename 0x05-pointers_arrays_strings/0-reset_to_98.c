93ed2a82635574d992879d5b28b28ac2d99dc2b6
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-24 11:20:58 -0700

    Start of 0x05-pointers_arrays_strings Also, added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - resets the given variable to 98
+ * @n: The number to be reset to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+## Project directory for Pinters_Arrays_Strings
+File 0-reset_to_98.c - A program that updates the value pointed by a pionter
