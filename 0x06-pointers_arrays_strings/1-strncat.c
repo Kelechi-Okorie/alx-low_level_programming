980c4145e92af507abd114025ea37d94238398b7
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-26 12:58:02 -0700

    Add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * _strncat - concatenates two strin
+ * @dest: the string to concatenate to
+ * @src: the string to concatenate from
+ *
+ * Return: pointer to char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int dest_len;
+
+	i = 0;
+	dest_len = 0;
+	while (dest[i] != '\0')
+	{
+		dest_len++;
+		i++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 ## Project folder for 0x06_pointers_arrays_sting project
 
 File 0-strcat.c - A program that concatenates 2 stings
+File 1-strncat.c - A program that concatenates n characters on one string to another string
