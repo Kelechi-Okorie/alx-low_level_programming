6f6d8cc59f3b1d2e6d2bf6cf7f7e847c76018528
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-27 13:22:11 -0700

    Bugfix 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,18 @@
 char *leet(char *s)
 {
 	int i;
+	int j;
 
-	i = 0;
-	while (s[i] != '\0')
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-
-		i++;
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
 	}
 
 	return (s);
