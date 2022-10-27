9c438b3085aead011b1e82142ff55cd48dcb7944
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-27 12:36:54 -0700

    Bugfix 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] == s2[i])
-			continue;
+		if (*s1 == '\0')
+			return (0);
 
-		if (s1[i] > s2[i])
-			return (15);
-		else
-			return (-15);
+		s1++;
+		s2++;
 	}
-
-	if (s1[i] != '\0')
-		return (15);
-
-	if (s2[i] != '\0')
-		return (-15);
-
-	return (0);
+	return (*s1 - *s2);
 }
