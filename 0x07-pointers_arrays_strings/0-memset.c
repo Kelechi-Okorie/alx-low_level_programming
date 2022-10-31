413fa083220e98da2c5d1912b9b998b95d0f0216
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-10-31 11:21:53 -0700

    Add 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the buffer to fill
+ * @b: the byte to fill the buffer with
+ * @n: the number of bytes to fill in the buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+
+		i++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+## README file for 0x07 Pointers, Arrays and Strings
+
+File 0-memset.c - A program that fills memory with a constant byte
