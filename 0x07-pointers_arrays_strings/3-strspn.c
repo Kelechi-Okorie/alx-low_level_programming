9b912015616608e89befe3779a06f8fad5e4656c
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-01 12:28:28 -0700

    Bugfix on 3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@
  *
  * Return: number of bytes in @s before first mistmatch
  */
-
+/*
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept)
@@ -46,4 +46,30 @@ char *_strchr(char *s, char c)
 		if (i == 0)
 			return (NULL);
 	}
+}*/
+
+unsigned int _strspn( char *s, char *accept)
+{
+	int i;
+	int j;
+	int f;
+	int flag;
+
+	f = 0;
+	for (i  = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			return (f);
+	}
+
+	return (0);
 }
