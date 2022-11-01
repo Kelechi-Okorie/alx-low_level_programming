f08b6bb1fe55330169d6eea63589cb348d252aca
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-01 12:31:34 -0700

    Bugfix on 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,47 +7,6 @@
  *
  * Return: number of bytes in @s before first mistmatch
  */
-/*
-char *_strchr(char *s, char c);
-
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int i;
-	unsigned int j;
-	unsigned int k;
-	char *p;
-
-	i = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-		p = _strchr(accept, s[i]);
-		if (p != NULL)
-			j++;
-		else
-			return j;
-
-		i++;
-	}
-
-	return j;
-}
-
-char *_strchr(char *s, char c)
-{
-	int i;
-
-	i = 0;
-	while(1)
-	{
-		i = *s++;
-		if (i == c)
-			return (s - 1);
-		if (i == 0)
-			return (NULL);
-	}
-}*/
-
 unsigned int _strspn( char *s, char *accept)
 {
 	int i;
