313d0fec270dee882a0a7306e141f9ff19beadc7
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-02 11:04:19 +0000

    Bugfix 7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,21 @@
+#include "main.h"
+
 /**
- * main - check the code
+ * print_chessboard - prints the chessboard
+ * @a: 2D array of containing the chessboard
  *
- * Return: al
+ * Return: always void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
+}
