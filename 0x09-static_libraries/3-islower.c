7adfd046399a422bccb060d9522472d101f151c2
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-07 00:14:36 -0800

    Create static library libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if the argument is lowercase character
+ * @c: The argument to check
+ *
+ * Return: 1 if @c is lowercase character
+ * 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always void
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kelvin Mitnick");
+	return (0);
+}
