a10ce8f60ccd8d35f0a5037c976e16b64eb63a77
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-10 18:56:25 +0000

    Add 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates 2 strings
+ * @s1: base string
+ * @s2: string to add to base string
+ * @n: number of strings to add fron @s2
+ *
+ * Return: Pointer to char if successful,
+ * Null otherwise
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr = NULL;
+	unsigned int i;
+	unsigned int len1;
+	unsigned int len2;
+
+	len1 = 0;
+	len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
+	if (n >= len2)
+		n = len2;
+
+	ptr = malloc(sizeof(len1 + n) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[len1 + i] = s2[i];
+	}
+	ptr[len1 + i] = '\0';
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 ## Readme file for 0x0c-more_malloc project
 
-File 0-malloc_checked.c - file that allocates memory using malloc
+File 0-malloc_checked.c - A program that allocates memory using malloc
+File 1-string_nconcat.c -A program that concatenates two strings
