dc084bd4e14fb8acf01e054b10fc035fee3739fd
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-14 16:16:09 +0000

    Add 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog variable
+ * @d: dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog owner
+ *
+ * Description: Initializes a dog struct @d with
+ * the provided parameters
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 ## Readme file for 0x0E-structures_typedef
 
 File dog.h A program that defines a struct
+File 1-init_dog.c - A program that initialize a variable of type struct dog
