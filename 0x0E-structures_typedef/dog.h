7aefafc5c59588514bbf3b433c9990415c454de0
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-14 15:48:21 +0000

    Bugfix on dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name dog's owner
+ *
+ * Description: This is a dog struct
+ */
 struct dog
 {
 	char *name;
