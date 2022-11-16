21574be3dc5aa52c8f8535bcf645d0bff4b60c3a
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-16 13:51:15 +0000

    Add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the function to call
+ *
+ * Return: Always nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x0F-function_pionters
+
+File 0-print_name.c - prints a name
