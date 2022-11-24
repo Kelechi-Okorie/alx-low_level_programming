4a1302270459734a00b1d9ce3fd6e552af302174
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-24 17:43:33 +0000

    Add 0-print_list.c

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# README file for 0x12-single_linked_list
+
+File 0-print_list.c - program that prints all elements of a list
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (mallo'ed string)
+ * @len: length of the string
+ * @next: points tot he next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* _LIST_H */
