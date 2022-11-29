1e346ad2169c18788fd168303a300cce7243b1f0
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-11-29 16:51:26 +0000

    bugfix on 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,13 +31,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (i <= idx - 1 && temp->next)
+	while ((i <= idx - 1) && temp->next)
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	if (i < idx - 1 || !(temp->next))
+	if ((i < idx - 1) || !(temp->next))
 		return (NULL);
 
 	new->next = temp->next;
