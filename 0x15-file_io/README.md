e1691ced4b3fcf8959735239076132aa3344ae4e
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-12-05 14:46:11 +0000

    Add 0-read_textfile.c

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x15-file_io
+
+File 0-read_textfile.c - A program that reads a text file and prints it to the POSIX standard output
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _MAIN_H */
