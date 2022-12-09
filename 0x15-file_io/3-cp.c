c3af8a040a00eb9c87656e290f41f0f33db5a92c
Kelechi Okorie <kelechiokorie22@gmail.com>
2022-12-09 12:35:57 +0000

    Bugfix 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	b = close(fd_w);
 	if (a < 0 || b < 0)
 	{
-		if (a < 0))
+		if (a < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (b < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
