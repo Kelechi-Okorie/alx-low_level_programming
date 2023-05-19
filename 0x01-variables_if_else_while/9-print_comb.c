174b57aa7dd0251d5b8bef597a5045576af3d339
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-05-19 14:06:14 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of single-digit numbers
+ *
+ * Description: printsll all possible combinations
+ * of single-digit numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+	while (i++ < '9')
+	{
+		putchar(i);
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -8,3 +8,4 @@ File 5-print_numbers.c - prints all single digit numbers of base 10
 File 6-print_numberz.c - prints all single digit numbers of base 10
 File 6-print_tebapla.c - prints the lowercase alphabet in reverse
 File 8-print_base16.c - prints all the numbers of base 16 in lowercase
+File 9-print_comb.c - print all possible combinations of a single-digit numbers
