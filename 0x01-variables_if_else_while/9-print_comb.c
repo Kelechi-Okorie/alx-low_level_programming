c55e43fa8cebdf03c22c40315dfe69ba52913fc6
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-05-19 14:11:28 +0000

    Hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	int i;
 
 	i = '0';
-	while (i++ < '9')
+	while (i <= '9')
 	{
-		putchar(i);
-		if (i < '9')
+		putchar(i++);
+		if (i <= '9')
 		{
 			putchar(',');
 			putchar(' ');
