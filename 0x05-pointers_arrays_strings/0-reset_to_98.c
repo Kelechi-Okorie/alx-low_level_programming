2ef10cacea65d4ec1f9babb75b9049143b3b1a10
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-05-29 11:27:57 +0000

    Added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets an int variable to 98
+ * @n: the variable to reset
+ *
+ * Description: resets @n to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# readme file for 0x05-pointers_array_strings
+File 0-reset_to_98.c - resets a variable to 98
