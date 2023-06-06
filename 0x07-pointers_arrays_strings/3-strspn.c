066df12c2df63cececa5deb0c68ce5fd80f362df
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-06 18:19:52 +0000

    Added 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	char *sp1, sp2;
+	char *sp1, *sp2;
 
 	for (sp1 = s; *sp1 != '\0'; sp1++)
 	{
-		for (sp2 = accept; s
+		for (sp2 = accept; ; sp2++)
+		{
+			if (*sp2 == '\0')
+				return (sp1 - s);
+			else if (*sp1 == *sp2)
+				break;
+		}
+	}
+
+	return (sp1 - s);
+}
