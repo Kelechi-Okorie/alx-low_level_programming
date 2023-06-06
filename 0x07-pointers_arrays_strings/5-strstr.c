c7b24fd7714b19da7ee160beb1489b974ef19f25
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-06 19:37:52 +0000

    Added 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stddef.h>
+
+char *_strchr(char *s, char c);
 
 /**
  * _strstr - locates a substring
@@ -12,4 +15,46 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	char *s, *sp1, *sp2;
+
+	s = haystack;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	for (; (s = _strchr(s, *needle)) != NULL; s++)
+	{
+		for (sp1 = s, sp2 = needle; ;)
+		{
+			if (*++sp2 == '\0')
+				return (s);
+			else if (*++sp1 != *sp2)
+				break;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the search string
+ * @c: the character to search
+ *
+ * Description: locates first occurrence of @c in @s
+ * Return: if found pointer to first occurrence of @c in @s
+ * NULL otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	char *sp;
+
+	for (sp = s; *sp != c; sp++)
+	{
+		if (*sp == '\0')
+			return (NULL);
+	}
+
+	return (sp);
+}
 
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,7 +1,7 @@
-# Readme file for 0x07-pointers_arrays_strings project
-File 0-memset.c -fills memory with a constant byte
-File 1-memcpy - copies memory area
-File 2-strchr.c - locates a character in a string
-File 3-strspn.c - gets the length of a prefix substring
-File 4-strpbrk.c - searches a string for a set of bytes
-File 5-strstr.c - locates a substring in a string
+# Readme file for 0x07-pointers_arrays_strings project  
+File 0-memset.c -fills memory with a constant byte  
+File 1-memcpy - copies memory area  
+File 2-strchr.c - locates a character in a string  
+File 3-strspn.c - gets the length of a prefix substring  
+File 4-strpbrk.c - searches a string for a set of bytes  
+File 5-strstr.c - locates a substring in a string  
