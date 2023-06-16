de0aa3f48830dbd4397c6ab059549d945cd5e546
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-16 06:41:54 +0000

    Added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Description: duplicates a string and returs a
+ * pointer to the duplicate
+ * Return: Pointer to duplicated string if successful
+ * Null otherwisw
+ */
+char *_strdup(char *str)
+{
+	char *s, *sp1, *sp2;
+	int n;
+
+	if (!str)
+		return (NULL);
+
+	for (sp1 = str; *sp1 != '\0'; sp1++)
+		;
+
+	n = sp1 - str;
+
+	s = (char *) malloc(n * sizeof(char));
+
+	if (!s)
+		return (NULL);
+
+	for (sp1 = str, sp2 = s; (*sp2 = *sp1) != '\0'; sp1++, sp2++)
+		;
+
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 #The readme file for 0x0B-malloc_free  
 
-File 0-create_array.c - creates an array of chars
+File 0-create_array.c - creates an array of chars  
+File 1-strdup.c - duplicates a string
