c2dd569548a9d722f5b181bfe2f5804040f90c90
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-17 17:32:24 +0000

    Hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (!ptr)
+	{
 		p = malloc(new_size);
+		return (p);
+	}
 
 	if (new_size == 0 && ptr)
 	{
