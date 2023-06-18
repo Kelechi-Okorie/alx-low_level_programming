03f78b766bf66f467526085accb4f1d4e357e41d
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-18 08:54:05 +0000

    added 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to print
+ *
+ * Description: prints the string @s using
+ * recursion
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x08-recursion
+
+File 0-puts_recursion.c - prints a string
