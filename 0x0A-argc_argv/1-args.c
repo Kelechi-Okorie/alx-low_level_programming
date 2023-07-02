d599b36dde1a38d90ebed4905261ef3376ef7717
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-07-02 17:24:17 +0000

    Added 1-nargs.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to program
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Description: prints number of arguments passed to program
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 # Added readme.md
 
-File 0-whatsmyname.c - A program that prints itself
+File 0-whatsmyname.c - A program that prints itself  
+File 1-args.c - Prints number of arguments passed to program
