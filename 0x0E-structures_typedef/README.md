f6fb07b10a96b1a6cd59cf6e50486af37682ad54
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-07-02 19:50:14 +0000

    Added dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x0E-structures_typedef
+
+File dog.h - defines a dog struct  
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * struct dog - a dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog owner
+ *
+ * Description: defines a dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _MAIN_H */
