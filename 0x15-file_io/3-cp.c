0654d5a263460b690877aa982b061f9bfa5ef51e
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-07-12 16:23:05 +0000

    Hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#define SIZE 1024
+#include "3-cp.h"
 
 /**
  * main - copies content from one file to another
  * @argc: argument count
  * @argv: argument vector
  *
- * Description: copies content from one file to another
  * Return: status code indicating the status of the operation
  */
 int main(int argc, char *argv[])
 {
 	int from, to, n, a, b;
-	char buf[SIZE];
+	char buf[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-	       exit(97);
+		exit(97);
 	}
-
 	from = open(argv[1], O_RDONLY);
 	if (from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to < 0)
+	while ((n = read(from, buf, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((n = read(from, buf, SIZE)) > 0)
-	{
-		if (write(to, buf, n) != n)
+		if (to < 0 || write(to, buf, n) != n)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(from);
-			close(to);
 			exit(99);
 		}
+	}
+	if (n < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	a = close(from);
 	b = close(to);
@@ -57,6 +45,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", a == 0 ? a : b);
 		exit(100);
 	}
-
 	return (0);
 }
