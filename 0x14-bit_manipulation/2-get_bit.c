4fda392a49f8f55fd2e087e55336f652e2fec13e
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-03 15:02:04 +0000

    Added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @n: the number to check
+ * @index: the index to return the bit of
+ *
+ * Description: returns value of a bit at a given index
+ * Return: the value the bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask, bit;
+
+	if (index < 0)
+		return (-1);
+
+	mask = n >> index;
+
+	bit = mask & 1;
+
+	return (bit);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,4 +1,5 @@
 # Readme file for bit manipulation project
 
 File 0-binary_to_uint.c - converts a binary number to an unsigned int  
-File 1-print_binary.c - prints binary from unsigned int
+File 1-print_binary.c - prints binary from unsigned int  
+File 2-get_bit.c - returns the value of a bit at a given index
