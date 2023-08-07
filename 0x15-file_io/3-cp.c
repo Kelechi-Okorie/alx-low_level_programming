af170f1cd196d708ba450357c6672520d3815a14
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-07 23:04:27 +0000

    Added 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define SIZE 1024
+
+/**
+ * main - copies content of a file a another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: copies the contents of one file to another
+ * Return: status codes to indicate status of operation
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, c_from, c_to, n;
+	char buf[SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((n = read(fd_from, buf, SIZE)) > 0)
+	{
+		if (fd_to == -1 || (write(fd_to, buf, n) != n))
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			if (fd_to > 0)
+				close(fd_to);
+			exit(99);
+		}
+	}
+	if (n == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+	c_from = close(fd_from);
+	c_to = close(fd_to);
+	if (c_from == -1 || c_to == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", c_from == -1 ? c_from : c_to);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,4 +2,5 @@
 
 File 0-read_textfile.c - reads a text file and prints it to the POSIX standard output  
 File 1-create_file.c - creates and writes to a file  
-File 2-append_text_to_file.c - appends text at the end of a file
+File 2-append_text_to_file.c - appends text at the end of a file  
+File 3-cp.c - copies the content of one file to another
