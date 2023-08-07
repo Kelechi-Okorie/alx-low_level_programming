88d5e5470dcc8ec3dd6869db9dc10d8c99694fbe
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-07 14:20:25 +0000

    Added 1-creat_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file and write into it
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to file
+ *
+ * Description: creates a file and writes to it
+ * Return: On success 1 is returned
+ * 0 is returned on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, num_write, n;
+
+	num_write = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content)
+	{
+		for (n = 0; text_content[n] != '\0'; n++)
+			;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (0);
+
+	if (n > 0)
+	{
+		num_write = write(fd, text_content, n);
+	}
+
+	if (num_write < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,4 @@
 # The readme file for 0x15-file_io project
 
-Filel 0-read_textfile.c - reads a text file and prints it to the POSIX standard output  
+File 0-read_textfile.c - reads a text file and prints it to the POSIX standard output  
+File 1-create_file.c - creates and writes to a file
