54a6f1e295c60f4370e37354d34bb27699932247
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-07 14:37:02 +0000

    Hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,19 +21,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content)
-	{
-		for (n = 0; text_content[n] != '\0'; n++)
-			;
-	}
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (0);
 
-	if (n > 0)
+	if (text_content)
 	{
-		num_write = 0;
+		for (n = 0; text_content[n] != '\0'; n++)
+			;
+
 		num_write = write(fd, text_content, n);
 
 		if (num_write < n)
