ee5ceb15681e838f453159758c77bfb0dca56393
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-21 17:37:12 +0000

    Added 100-operatins.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - adds 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a x b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns modulus of a number
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus of a number
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,3 +1,5 @@
 # Readme file for 0x18-dynamic_libraries
 
-File libdynamics.so - dynamic library containing some listed
+File libdynamics.so - dynamic library containing some listed  
+file 1-create_dynamic_lib.sh - a shell script that creates a dynamic library  
+File 100-operations.so - A C so file to be called by python
