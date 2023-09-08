3ca34f0abf5a0b53614a50764f61648f8bf3fb4b
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-08 19:08:36 +0000

    Added 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Description: creates a hash table of a given size
+ * Return: pointer to newly created hash table on success
+ * NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t **array;
+	hash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+
+	array = calloc((size_t)size, sizeof(hash_node_t *));
+	if (!array)
+		return (NULL);
+
+	ht->array = array;
+
+	return (ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x1A-hash_tables project
+
+File 0-hash_table_create.c - creates a hash table
