5f4fc7b46ccb4ec88e1bc6e0af70057bb9c7b86f
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-09 16:23:15 +0000

    Added 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,64 @@
 #include "hash_tables.h"
+#include <string.h>
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
 
 /**
 * hash_table_set - adds an element to the hash table
+* @ht: the hash_table
+* @key: the key
+* @value: the value associated with the key
+*
+* Description: adds an element to the hash table
+* Return: 1 if it succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (!ht || !key || !strcmp(key, "") || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = add_node(&ht->array[index], key, value);
+	if (!node)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * add_node - adds a new node to the array index
+ * @head: head of the linked list referenced by the array index
+ * @key: the key of the node
+ * @value: the value associated with the key
+ *
+ * Description: adds a new node to the array index
+ * Return: the new node on success, NULL on failure
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *p;
+
+	p = malloc(sizeof(hash_node_t));
+	if (!p)
+		return (NULL);
+
+	p->key = strdup(key);
+	p->value = strdup(value);
+	p->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = p;
+	}
+	else
+	{
+		p->next = *head;
+		*head = p;
+	}
+
+	return (*head);
+}
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -10,9 +10,11 @@
  */
 int main(void)
 {
-	hash_tables *ht;
+	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
 	hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "hetairas", "Kelechi");
+	hash_table_set(ht, "mentioner", "Okorie");
 	return (EXIT_SUCCESS);
 }
