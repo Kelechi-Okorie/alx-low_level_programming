33afc1212ace777111b090a3057c5e242a1b21db
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-12-12 17:08:05 +0100

    Bug fix for task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,6 +14,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (!array || size == 0)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d]\n", array[i]);
